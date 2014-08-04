498d92fdf721871107fcefddcd52cf3ffe3c6453
Kyle Swanson <kylophone@gmail.com>
2014-08-04 17:19:54 -0500

    Create xplay.py

---
+++ xplay.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import sys, os
+RTPDUMP_BIN = "rtpdump" #change this if your path is different
+PLAY_BIN = "play" #change this if your path is different
+
+if len(sys.argv) != 2:
+	print "Please supply a valid Livewire channel number (1 - 32767). Correct usage: xplay 32767"
+	sys.exit(1)
+else:
+	multicastAddr = int(sys.argv[1]) + 0xEFC00000 #Axia channel number + base IP (239.192.0.0 [in hex]) 
+	os.system(RTPDUMP_BIN + " -F payload " + hex(multicastAddr) + "/5004 | " + PLAY_BIN + " -c 2 -r 48000 -b 24 -e signed-integer  -B -t raw -")
